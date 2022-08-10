be242d9d58ec4d6298800d1cc0ac640eb76b0753
fmgold <fmgold2015@gmail.com>
2022-08-10 16:20:44 +0100

    feat: added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E31B23",
+        secondary: "blue",
+        indigo: "#0E0E2C",
+        body: "#4A4A68",
+        subtle: "#8C8CA2",
+        subtitle: "#8C8CA1",
+        success: "#1CB66C",
+        accent: "#ECF1F4",
+        background: "#F8F8F8",
+        "iris-10": "#EFEFFD",
+        "iris-20": "#ECECFB",
+        iris: "#4B4DED",
+        "light-pink": "#F4A2A5",
+        "red-mild": "#FF7B93",
+      },
+    },
+  },
+  plugins: [],
+};
