f7a0770ff4446383d7040f885e0d788ea14b0f42
fmgold <fmgold2015@gmail.com>
2022-08-11 08:43:42 +0100

    adding of packages

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import tw from "./tailwind";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`flex flex-1 justify-center items-center`}>
+      <Text style={tw`text-3xl text-primary font-semibold`}>Shakur </Text>
+      <Text style={tw`text-lg text-body font-semibold`}>
+        Let get it done bro{" "}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
